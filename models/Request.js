0d26296b7f5987288b9efcff6674c50210090a60
Obito2002 <anshuman020202@gmail.com>
2022-11-28 01:02:52 +0530

    server changes

--- models/Request.js
+++ models/Request.js
@@ -25,6 +25,14 @@ const Req = new mongoose.Schema({
         type:String,
         required:true,  
     },
+    lat:{
+        type:String,
+        required:true
+    },
+    lng:{
+        type:String,
+        required:true
+    }
 },
 {timestamps: true}
 )
